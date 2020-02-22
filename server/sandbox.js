60bae7b1fe0c5d57feb4e1edb2f39b34d079c92f
Adrien Turchini <adrien.turchini@me.com>
2020-02-22 10:40:52 +0100

    little correction

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-console, no-process-exit */
 const michelin = require('./bibgourmand');
+const maitrerestaurateurs = require('./maitrerestaurateurs');
 
-//const michelin = require('./maitrerestaurateurs')
-
-
-testMR = 'https://www.maitresrestaurateurs.fr/annuaire/recherche';
+mr = 'https://www.maitresrestaurateurs.fr/annuaire/recherche';
 maitre = 'https://www.maitresrestaurateurs.fr/profil/396';
 bib = 'https://guide.michelin.com/fr/fr/centre-val-de-loire/veuves/restaurant/l-auberge-de-la-croix-blanche';
 
@@ -14,11 +12,11 @@ var fs = require('fs');
 
 
 
-async function sandbox (searchLink = bib) {
+async function sandbox (searchLink = maitre) {
   
   try {
     console.log(`🕵️‍♀️  browsing ${searchLink} source`);
-    const restaurant = await michelin.scrapeRestaurant(searchLink);
+    const restaurant = await maitrerestaurateurs.scrapeRestaurant(searchLink);
     
     
     var fs = require('fs');
@@ -35,11 +33,7 @@ async function sandbox (searchLink = bib) {
     obj.push({
       restaurant
     })
-*/
-   
-  
-
-    
+*/    
     fs.writeFileSync('./data.json', JSON.stringify(restaurant, null, 4), (err) => {
       if (err) {
         console.error(err);
